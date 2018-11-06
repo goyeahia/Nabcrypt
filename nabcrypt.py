2d33d6998affc1bd57e39c3fc8d76437f156aaeb
Yeahia Sarker <40637938+goyeahia@users.noreply.github.com>
2018-11-06 15:23:25 +0600

    Removed features

--- nabcrypt.py
+++ nabcrypt.py
@@ -25,35 +25,15 @@ class nabcrypt:
         if sys.argv[1].endswith(".nab"):
             self.decrypt()
         else:
-            for file in os.listdir():
-                if file == sys.argv[1]:
-                    self.encrypt()
-                else:
-                    self.create_and_encrypt()
+            self.encrypt()
     def encrypt(self):
-        #with open(str(sys.argv[1])) as f:
-        #mode = pyaes.AESModeOfOperationCTR(self.key)
         with open(str(sys.argv[1])) as input_file:
             with open(str(sys.argv[1]) + "nab") as output_file:
                 pyaes.encrypt_stream(self.mode, input_file, output_file)
     def decrypt(self):
         output_file_name = str(sys.argv[1].split(".")[0])
         with open(str(sys.argv[1])) as input_file:
-        #print(subprocess.Popen(["echo", "File has been saved"], stdout=subprocess.PIPE).communicate())
             with open(str(output_file_name)) as output_file:
                 pyaes.decrypt_stream(self.mode, input_file, output_file)
-    def create_and_encrypt(self):
-        with open(str(sys.argv[1])) as input_file:
-            print("Editor mode is on")
-            print("Press CTRL + D to save and exit")
-            while True:
-                input_data = sys.stdin.read()
-                if input_data == "pet":
-                    break
-            print(input_data)
-            print(type(input_data))
-            input_file.write(input_data)
-            with open(str(sys.argv[1]) + "nab") as output_file:
-                pyaes.encrypt_stream(self.mode, input_file, output_file)
 nabcrypt = nabcrypt()
 nabcrypt.create_file()
